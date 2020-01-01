0df2b59a3f1a3ba642a9867d1b6b601e028f33c0
Jeymsson <jeymsson@hotmail.com>
2020-01-01 19:58:02 -0300

    Implementing sessions

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: '45cde2bc7c6375c1fd412dfebb332d0c',
+    expiresIn: '1d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
@@ -7,5 +8,6 @@ routes.get('/', async (req, res) => {
     return res.json({ Hello: 'world' });
 });
 routes.post('/user', UserController.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
